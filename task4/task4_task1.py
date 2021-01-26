69d253e8c9556ce323585fa741b131b70a2445e3
GlebShev <77424538+GlebShev@users.noreply.github.com>
2021-01-26 23:58:12 +0300

    Delete task4 directory

--- task4/task4_task1.py
+++
@@ -1,5 +0,0 @@
-import sys
-hours, salary, bonus = map(sys.argv, float)
-print(f"Зарабатная плата: {hours*salary + bonus}")
-
-
--- task4/task4_task5.py
+++
@@ -1,7 +0,0 @@
-import functools
-def my_func(el0, el_next):
-    return el0 * el_next
-
-my_list = [i for i in range(99, 1001) if i%2 == 0]
-answer = [functools.reduce(my_func, [i for i in my_list])]
-print(answer)
